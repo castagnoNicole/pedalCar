09bd22b6201c178d4aeead982e1b17c622c9a009
Alex <147047219+4lex42@users.noreply.github.com>
2024-02-07 12:13:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,54 +2,71 @@
 // Accelerometers to control the Servo Motor (needs to be finalized)
 //------------------------------------------------------------
 
-var accelButton = document.getElementById("accelPermsButton");
-var operatingSystem = "";
+var accelButton = document.getElementById('accelPermsButton');
+var operatingSystem = ""
 
-if (typeof DeviceMotionEvent.requestPermission === "function") {
+if (typeof DeviceMotionEvent.requestPermission === 'function') {
   // iOS 13+
-  accelButton.disabled = false;
-  accelButton.innerHTML = "Get Accelerometer Permissions (iOS : use Bluefy)";
-  operatingSystem = "iOS";
-} else if (window.DeviceMotionEvent != undefined) {
-  // Android
-  accelButton.disabled = false;
-  accelButton.innerHTML = "Use Accelerometer (Android : use Google Chrome)";
-  operatingSystem = "Android";
-} else {
-  // Non supported devices
-  accelButton.disabled = true;
-  accelButton.innerHTML = "Use Accelerometer (Not supported on your device)";
-  operatingSystem = "other";
+  accelButton.disabled = false
+  accelButton.innerHTML = "Get Accelerometer Permissions (iOS : use Bluefy)"
+  operatingSystem = "iOS"
 }
+else if (window.DeviceMotionEvent != undefined) {
+  // Android
+  accelButton.disabled = false
+  accelButton.innerHTML = "Use Accelerometer (Android : use Google Chrome)"
+  operatingSystem = "Android"
+}
+else {
+  // Non supported devices
+  accelButton.disabled = true
+  accelButton.innerHTML = "Use Accelerometer (Not supported on your device)"
+  operatingSystem = "other"
+
+}
+
+var rotation_degrees = 0;
+var frontToBack_degrees = 0;
+var leftToRight_degrees = 0;
 
 var servoAngle = 90;
 
 function getAccel() {
   if (operatingSystem == "iOS") {
-    DeviceMotionEvent.requestPermission().then((response) => {
-      if (response == "granted") {
+    DeviceMotionEvent.requestPermission().then(response => {
+      if (response == 'granted') {
         getServoAngle();
       }
     });
-  } else if (operatingSystem == "Android") {
+  }
+  else if (operatingSystem == "Android") {
     getServoAngle();
+
   }
 }
 
 function getServoAngle() {
-  // Add an event listener to listen for device orientation changes
-  window.addEventListener("deviceorientation", (event) => {
-    // Retrieve orientation angles from the event object
-    let beta = event.beta; // Z-axis rotation (in degrees)
+  // Add a listener to get smartphone orientation 
+  // in the alpha-beta-gamma axes (units in degrees)
+  window.addEventListener('deviceorientation', (event) => {
+    // Expose each orientation angle in a more readable way
+    let alpha = event.alpha;    
+    let beta = event.beta;
+    let gamma = event.gamma;
 
-    // Get the needle element
-    let needle = document.getElementsByClassName("needle")[0];
+    // Constrain the alpha angle within a certain range (e.g., 0 to 360 degrees)
+    alpha = Math.max(0, Math.min(360, alpha));
 
-    // Update the rotation of the needle based on the alpha angle
-    needle.style.transform = "rotate(" + beta + "deg)";
+    let needle = document.getElementsByClassName("needle")[0]    
+    needle.setAttribute('style', "background: red");
+    // Calculate the rotation angle for the needle within the defined range
+    let rotationAngle = alpha; // Assuming alpha represents the desired rotation angle
 
-    // Store the orientation angle for further use (e.g., BLE transmission)
-    servoAngle = parseInt(beta); // Convert alpha angle to an integer
+    // Update the needle's rotation only if it's within the allowed range
+    if (rotationAngle >= 0 && rotationAngle <= 360) {
+      needle.style.transform = "rotate(" + rotationAngle + "deg)";
+      servoAngle = parseInt(rotationAngle); // Update the servo angle
+    }
   });
 }
 
@@ -60,34 +77,35 @@ function getServoAngle() {
 
 var motor_speed = 0;
 
+
 //------------------------------------------------------------
 // BLE communication handling
 //------------------------------------------------------------
 
 // Get references to UI elements
-let connectButton = document.getElementById("connect");
-let disconnectButton = document.getElementById("disconnect");
-let terminalContainer = document.getElementById("terminal");
-let sendForm = document.getElementById("send-form");
-let inputField = document.getElementById("input");
+let connectButton = document.getElementById('connect');
+let disconnectButton = document.getElementById('disconnect');
+let terminalContainer = document.getElementById('terminal');
+let sendForm = document.getElementById('send-form');
+let inputField = document.getElementById('input');
 
 var myInterval;
 
 // Connect to the device on Connect button click
-connectButton.addEventListener("click", function () {
+connectButton.addEventListener('click', function () {
   connect();
 });
 
 // Disconnect from the device on Disconnect button click
-disconnectButton.addEventListener("click", function () {
+disconnectButton.addEventListener('click', function () {
   disconnect();
 });
 
 // Handle form submit event
-sendForm.addEventListener("submit", function (event) {
+sendForm.addEventListener('submit', function (event) {
   event.preventDefault(); // Prevent form sending
   send(inputField.value); // Send text field contents
-  inputField.value = ""; // Zero text field
+  inputField.value = ''; // Zero text field
   inputField.focus(); // Focus on text field
 });
 
@@ -95,52 +113,53 @@ sendForm.addEventListener("submit", function (event) {
 let deviceCache = null;
 
 // Launch Bluetooth device picker and connect to the selected device
+//Here I use two things to make it easier to read :
+//	1. I use p.then when p is a Promise which force operations to follow a certain order (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises) and (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then)
+//		With the syntax p.then(onFulfilled[, onRejected]);  Here, basically, we want sucesses to continue, otherwise we send an error.
+//	2. I use arrow function (https://www.w3schools.com/js/js_arrow_function.asp) where :
+//		function (s) { return s.length }
+//			simply becomes :
+//		s => s.length			(return by default if no {}, just giving parameters (s here) and no name
 function connect() {
-  return (deviceCache ? Promise.resolve(deviceCache) : requestBluetoothDevice())
-    .then((device) => connectDeviceAndCacheCharacteristic(device))
-    .then((characteristic) => startNotifications(characteristic))
-    .then((myInterval = setInterval(sendingBLEinfo, 100)))
-    .catch((error) => log(error));
+  return (deviceCache ? Promise.resolve(deviceCache) :
+    requestBluetoothDevice()).
+  then(device => connectDeviceAndCacheCharacteristic(device)).
+  then(characteristic => startNotifications(characteristic)).
+  then(myInterval = setInterval(sendingBLEinfo, 100)).
+  catch(error => log(error));
 }
 
 function requestBluetoothDevice() {
-  log("Requesting bluetooth device...");
+  log('Requesting bluetooth device...');
 
-  return navigator.bluetooth
-    .requestDevice({
-      filters: [
-        {
-          services: [0x2365],//0x2365
-        },
-      ],
-    })
-    .then((device) => {
-      log('"' + device.name + '" bluetooth device selected');
-      deviceCache = device;
+  return navigator.bluetooth.requestDevice({
+    filters: [{
+      services: [0x2365] //[0xFFE0]
+    }],
+  }).
+  then(device => {
+    log('"' + device.name + '" bluetooth device selected');
+    deviceCache = device;
 
-      // Added line
-      deviceCache.addEventListener(
-        "gattserverdisconnected",
-        handleDisconnection
-      );
+    // Added line
+    deviceCache.addEventListener('gattserverdisconnected',
+      handleDisconnection);
 
-      return deviceCache;
-    });
+    return deviceCache;
+  });
 }
 
 function handleDisconnection(event) {
   let device = event.target;
 
-  log(
-    '"' +
-      device.name +
-      '" bluetooth device disconnected, trying to reconnect...'
-  );
+  log('"' + device.name +
+    '" bluetooth device disconnected, trying to reconnect...');
 
-  connectDeviceAndCacheCharacteristic(device)
-    .then((characteristic) => startNotifications(characteristic))
-    .catch((error) => log(error));
+  connectDeviceAndCacheCharacteristic(device).
+  then(characteristic => startNotifications(characteristic)).
+  catch(error => log(error));
 }
+
 
 // Characteristic object cache
 let characteristicCache = null;
@@ -151,39 +170,37 @@ function connectDeviceAndCacheCharacteristic(device) {
     return Promise.resolve(characteristicCache);
   }
 
-  log("Connecting to GATT server...");
+  log('Connecting to GATT server...');
 
-  return device.gatt
-    .connect()
-    .then((server) => {
-      log("GATT server connected, getting service...");
+  return device.gatt.connect().
+  then(server => {
+    log('GATT server connected, getting service...');
 
-      return server.getPrimaryService(0xffe0);
-    })
-    .then((service) => {
-      log("Service found, getting characteristic...");
+    return server.getPrimaryService(0x2365);//0x2365
+  }).
+  then(service => {
+    log('Service found, getting characteristic...');
 
-      return service.getCharacteristic(0xffe1);
-    })
-    .then((characteristic) => {
-      log("Characteristic found");
-      characteristicCache = characteristic;
+    return service.getCharacteristic(0x0103);//0x0103
+  }).
+  then(characteristic => {
+    log('Characteristic found');
+    characteristicCache = characteristic;
 
-      return characteristicCache;
-    });
+    return characteristicCache;
+  });
 }
 
 // Enable the characteristic changes notification
 function startNotifications(characteristic) {
-  log("Starting notifications...");
+  log('Starting notifications...');
 
-  return characteristic.startNotifications().then(() => {
-    log("Notifications started");
+  return characteristic.startNotifications().
+  then(() => {
+    log('Notifications started');
     // Added line
-    characteristic.addEventListener(
-      "characteristicvaluechanged",
-      handleCharacteristicValueChanged
-    );
+    characteristic.addEventListener('characteristicvaluechanged',
+      handleCharacteristicValueChanged);
   });
 }
 
@@ -191,27 +208,23 @@ function disconnect() {
   clearInterval(myInterval); //stop sending data every time
   if (deviceCache) {
     log('Disconnecting from "' + deviceCache.name + '" bluetooth device...');
-    deviceCache.removeEventListener(
-      "gattserverdisconnected",
-      handleDisconnection
-    );
+    deviceCache.removeEventListener('gattserverdisconnected',
+      handleDisconnection);
 
     if (deviceCache.gatt.connected) {
       deviceCache.gatt.disconnect();
       log('"' + deviceCache.name + '" bluetooth device disconnected');
-    } else {
-      log(
-        '"' + deviceCache.name + '" bluetooth device is already disconnected'
-      );
+    }
+    else {
+      log('"' + deviceCache.name +
+        '" bluetooth device is already disconnected');
     }
   }
 
   // Added condition
   if (characteristicCache) {
-    characteristicCache.removeEventListener(
-      "characteristicvaluechanged",
-      handleCharacteristicValueChanged
-    );
+    characteristicCache.removeEventListener('characteristicvaluechanged',
+      handleCharacteristicValueChanged);
     characteristicCache = null;
   }
 
@@ -219,23 +232,22 @@ function disconnect() {
 }
 
 // Intermediate buffer for incoming data
-let readBuffer = "";
+let readBuffer = '';
 
 // Data receiving
 function handleCharacteristicValueChanged(event) {
   let value = new TextDecoder().decode(event.target.value); //We want to decode byte to text
 
-  for (let c of value) {
-    //we receve 20 bytes per 20 bytes, let's make one string until we reach \n
-    if (c === "\n") {
-      //Be careful, now we need to add a line break when we use the serial monitor
+  for (let c of value) { //we receve 20 bytes per 20 bytes, let's make one string until we reach \n
+    if (c === '\n') { //Be careful, now we need to add a line break when we use the serial monitor
       let data = readBuffer.trim();
-      readBuffer = "";
+      readBuffer = '';
 
       if (data) {
         receive(data);
       }
-    } else {
+    }
+    else {
       readBuffer += c;
     }
   }
@@ -243,19 +255,19 @@ function handleCharacteristicValueChanged(event) {
 
 // Received data handling
 function receive(data) {
-  log(data, "in");
+  log(data, 'in');
 }
+
 
 function writeToCharacteristic(characteristic, data) {
   characteristic.writeValue(new TextEncoder().encode(data));
 }
 
+
 // Output to terminal
-function log(data, type = "") {
-  terminalContainer.insertAdjacentHTML(
-    "beforeend",
-    "<div" + (type ? ' class="' + type + '"' : "") + ">" + data + "</div>"
-  );
+function log(data, type = '') {
+  terminalContainer.insertAdjacentHTML('beforeend',
+    '<div' + (type ? ' class="' + type + '"' : '') + '>' + data + '</div>');
 }
 
 function send(data, logging = true) {
@@ -265,7 +277,7 @@ function send(data, logging = true) {
     return;
   }
 
-  data += "\n";
+  data += '\n';
 
   if (data.length > 20) {
     let chunks = data.match(/(.|[\r\n]){1,20}/g);
@@ -277,22 +289,19 @@ function send(data, logging = true) {
         writeToCharacteristic(characteristicCache, chunks[i]);
       }, i * 100);
     }
-  } else {
+  }
+  else {
     writeToCharacteristic(characteristicCache, data);
   }
 
   if (logging) {
-    log(data, "out");
+    log(data, 'out');
   }
-}
-
-function sendingBLEinfo() {
-  //R = rotation (info on servo motor) ; M = Move (info on motor speed) ; < and > are start and end of packet
-  send("<R" + servoAngle + ">" + "<M" + motor_speed + ">", true);
 }
 
 // Get the speed slider element
 const speedSlider = document.getElementById("speedSlider");
+const formInput = document.getElementById("input");
 
 // Event listener to handle slider changes
 speedSlider.addEventListener("input", function () {
@@ -304,14 +313,15 @@ speedSlider.addEventListener("input", function () {
 // Event listener to handle slider release
 speedSlider.addEventListener("mouseup", function () {
   // Reset slider to minimum value when released
-  speedSlider.value = 0;
   // Call a function to handle release (e.g., stop sending speed)
-  handleSliderRelease();
 });
 
 // Function to handle speed change
 function handleSpeedChange(speed) {
+  let x = "90;" + speed;
   // Implement your logic to handle speed change (e.g., send speed via Bluetooth)
+  formInput.value = x;
+  sendForm.submit();
   console.log("Speed changed to:", speed);
 }
 
